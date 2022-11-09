4020a9fdc15bcc9fbf5f1286b8cd3978aee8472c
John White <johnawhiteit@gmail.com>
2022-11-09 00:37:41 +0000

    boto3 python training

---
+++ python_packages.py
@@ -0,0 +1,11 @@
+# install boto3
+pip install boto3
+
+# install latest version
+You are using pip version 18.1, however version 20.0.2 is available.
+You should consider upgrading via the 'pip install --upgrade pip' command.
+
+# list pip packages installed
+pip freeze
+
+
